05dfbf62e89f27092d733b7547d658a650f440ef
Marc Schultz <lunij@users.noreply.github.com>
2021-01-14 10:22:56 +0100

    WIP

--- Package.swift
+++ Package.swift
@@ -10,11 +10,13 @@ let package = Package(
     products: [
         .library(
             name: "GoogleMaps",
+            type: .dynamic,
             targets: [
                 "GoogleMaps",
                 "GoogleMapsBase",
                 "GoogleMapsCore",
-                "GoogleMapsM4B"
+                "GoogleMapsM4B",
+                "GoogleMapsDummy"
             ]
         ),
     ],
@@ -22,6 +24,7 @@ let package = Package(
         .binaryTarget(name: "GoogleMaps", url: "https://github.com/lunij/swift-package-converter/raw/google-maps/Frameworks/GoogleMaps.zip", checksum: "11f80eec3876f7e60f1e517fb43462a3165ff17ed0eb2572866a5c83cd637267"),
         .binaryTarget(name: "GoogleMapsBase", url: "https://github.com/lunij/swift-package-converter/raw/google-maps/Frameworks/GoogleMapsBase.zip", checksum: "7d38fcbb5e0a1e1d17cae81b7f8bd077cf971bf56f22afbef9d3578efd40fa0a"),
         .binaryTarget(name: "GoogleMapsCore", url: "https://github.com/lunij/swift-package-converter/raw/google-maps/Frameworks/GoogleMapsCore.zip", checksum: "4079ec611d0cb073957de8fdc35fb3b5864f0a2463e232b3cf8c3c258dd95f7d"),
-        .binaryTarget(name: "GoogleMapsM4B", url: "https://github.com/lunij/swift-package-converter/raw/google-maps/Frameworks/GoogleMapsM4B.zip", checksum: "0411eb84a314c0c02773d2decd64227d5de3cb438c5d458c80d579b9ff066d0a")
+        .binaryTarget(name: "GoogleMapsM4B", url: "https://github.com/lunij/swift-package-converter/raw/google-maps/Frameworks/GoogleMapsM4B.zip", checksum: "0411eb84a314c0c02773d2decd64227d5de3cb438c5d458c80d579b9ff066d0a"),
+        .target(name: "GoogleMapsDummy")
     ]
 )
